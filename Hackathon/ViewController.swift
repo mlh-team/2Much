e82bd0976cd199da23ae98185c4287df7b538a4e
Greg <gmac12greg@gmail.com>
2017-12-02 17:52:00 -0500

    Initial Commit

---
+++ Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by Greg  MacEachern on 2017-12-02.
+//  Copyright © 2017 Greg  MacEachern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
